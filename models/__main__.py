4ba9a67102fd591a82259239ba85f68e18b7b4b7
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2023-01-15 23:05:31 +0100

    add if __name__ predicate

--- models/__main__.py
+++ models/__main__.py
@@ -1,8 +1,6 @@
 import json
-
 import torch
 from torch_geometric.nn.models import LightGCN
-
 import config as cfg
 from models.model_trainer import train
 
@@ -18,7 +16,8 @@ model = LightGCN(
     num_layers=cfg.NUM_LAYERS
 )
 
-optimizer = torch.optim.Adam(params=model.parameters(), lr=cfg.LEARNING_RATE)
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-model.to(device)
-train(model, data, optimizer, NUMBER_OF_PLAYLISTS, cfg.K, cfg.EPOCHS, cfg.BATCH_SIZE)
+if __name__ == '__main__':
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=cfg.LEARNING_RATE)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
+    train(model, data, optimizer, NUMBER_OF_PLAYLISTS, cfg.K, cfg.EPOCHS, cfg.BATCH_SIZE)
